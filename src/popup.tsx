77da5e9262e61d7e73c14ac9cc686474adc68845
Ben Marden <benvmarden@gmail.com>
2023-02-04 15:10:29 -0800

    Add basic functionality to group tabs

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,19 +1,65 @@
-import { useState } from "react"
+import { useEffect, useReducer, useState } from "react"
+
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  // const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([])
+  const [tabGroups, setTabGroups] = useState<chrome.tabGroups.TabGroup[]>([])
+
+  // useEffect(() => {
+  //   chrome.tabs.query({ currentWindow: true }, (tabs) => {
+  //     setTabs(tabs)
+  //   })
+  // }, [])
+
+  const handleTabGroup = async () => {
+    const domains = new Map<string, chrome.tabs.Tab[]>()
+    const tabs = await chrome.tabs.query({ currentWindow: true })
+
+    // Get the base domain of each tab and put into the domains Set
+    tabs.forEach((tab) => {
+      const url = new URL(tab.url)
+
+      if (domains.has(url.hostname)) {
+        domains.get(url.hostname).push(tab)
+      } else {
+        domains.set(url.hostname, [tab])
+      }
+    })
+
+    for (const [domain, tabs] of domains) {
+      const group = await chrome.tabs.group({
+        tabIds: tabs.map((tab) => tab.id)
+      })
+      await chrome.tabGroups.update(group, { title: domain })
+    }
+  }
+
+  const handleUngroupTabs = async () => {
+    const currentGroupedTabs = await chrome.tabs.query({ currentWindow: true })
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+    <div className="w-52">
+      <div>
+        <ul>
+          {tabGroups &&
+            tabGroups.map((tabGrp) => <li key={tabGrp.id}>{tabGrp.title}</li>)}
+        </ul>
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          className="rounded bg-cyan-500 text-white hover:bg-cyan-700 p-2 m-2"
+          onClick={handleTabGroup}>
+          Group Tabs
+        </button>
+        <button
+          className="rounded bg-cyan-500 text-white hover:bg-cyan-700 p-2 m-2"
+          onClick={handleUngroupTabs}>
+          Ungroup Tabs
+        </button>
+      </div>
     </div>
   )
 }
